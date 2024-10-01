07c0d7d17a1ad5773e91bf9d36715ab08194a282
sergio-coti <cotiaulajava@outlook.com>
2024-10-01 11:00:27 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/JavaApiClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiClientesApplication.class, args);
+	}
+
+}
